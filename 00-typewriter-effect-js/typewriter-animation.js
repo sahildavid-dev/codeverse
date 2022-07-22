2003643f93cc9fa04d661ec9fc7da9cf3e25c484
Sahil David <sahildavid268@gmail.com>
2022-07-22 14:43:05 +0100

    Initial Commit - First code snippet as typewriter animation

---
+++ 00-typewriter-effect-js/typewriter-animation.js
@@ -0,0 +1,54 @@
+/**
+ * Function to apply typewriter animation.
+ * Also, updates the word after the next animation
+ */
+
+// Word to update 
+const words = ['Developer', 'Educator', 'Friend', 'Blogger'];
+
+// Variable to control animations
+let counter = 0;
+const timer = 1000;
+const typeTimer = 100;
+
+/**
+ * Function to apply animation that changes word after typewriter effect
+ */
+function applyAnimation () {
+	const target = document.querySelector('.js-target')
+	if (!target) { return; }
+
+	// Typewriter Animation
+	typeWriter(target, words[counter]);
+	counter++
+
+	if (counter >= words.length) {
+		counter = 0
+	}
+
+	if (counter < words.length) {
+		setTimeout(applyAnimation, timer)
+	}
+}
+
+/**
+ * Function to apply typewriter effect to the word
+ * @param {HTMLElement} target - element for updated animated word 
+ * @param {String} word - word to use for the animation
+ * @param {Number} count - count of letters in animation
+ */
+function typeWriter (target, word, count = 0) {
+	if (count === 0) {
+		target.innerHTML = ''
+	}
+	target.innerHTML += word[count]
+
+	if (count++ < word.length - 1) {
+		setTimeout(() => typeWriter(target, word, count), typeTimer)
+	}
+}
+
+/**
+ * Call the animation function
+ */
+applyAnimation();
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# code-verse
-Code Snippets from personal projects
+# Codeverse
+_Author: Sahil David, Last updated: 22/07/22_
+
+Codeverse is a universe or collection of various code snippets, that I have gathered over time during my development work on various projects. 
+
+Sharing is the key. We learn more when we learn together. Therefore, I include any piece of code that can help others learn and/or get ideas for their projects.
+
+---
+## Table of Content
+
+- [00 - Typewriter Effect JavaScript Animation](/00-typewriter-effect-js/)
+
+---
+## Contribute
+If you have a code to share, please feel free to fork the repo and submit a pull request. I look forward to your amazing code.
